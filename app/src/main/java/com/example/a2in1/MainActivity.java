d325d3eb2e571f96f7b261b3089fda8471d02a47
B00097070 <32622113+B00097070-Ryan-Noble@users.noreply.github.com>
2019-10-19 22:30:58 +0100

    Facebook Login (Continue with Facebook Button) working

--- app/src/main/java/com/example/a2in1/MainActivity.java
+++ app/src/main/java/com/example/a2in1/MainActivity.java
@@ -16,52 +16,11 @@ import static android.app.Activity.RESULT_OK;
 
 public class MainActivity extends Activity {
 
-    private static final int RESULT_PROFILE_ACTIVITY = 1;
-    private static final int RESULT_POSTS_ACTIVITY = 2;
-    private static final int RESULT_PERMISSIONS_ACTIVITY = 3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // If MainActivity is reached without the user being logged in, redirect to the Login
-        // Activity
-        if (AccessToken.getCurrentAccessToken() == null) {
-            Intent loginIntent = new Intent(this, FBLoginActivity.class);
-            startActivity(loginIntent);
+
         }
-
-        // Make a button which leads to profile information of the user
-        Button gotoProfileButton = findViewById(R.id.btn_profile);
-
-        gotoProfileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (AccessToken.getCurrentAccessToken() == null) {
-                    Intent profileIntent = new Intent(MainActivity.this, FBLoginActivity
-                            .class);
-                    startActivityForResult(profileIntent, RESULT_PROFILE_ACTIVITY);
-                }
-            }
-        });
-
-        // Make a logout button
-        Button fbLogoutButton = findViewById(R.id.btn_fb_logout);
-        fbLogoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LoginManager.getInstance().logOut();
-                Intent loginIntent = new Intent(MainActivity.this, FBLoginActivity.class);
-                startActivity(loginIntent);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            Toast.makeText(MainActivity.this, "E", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
