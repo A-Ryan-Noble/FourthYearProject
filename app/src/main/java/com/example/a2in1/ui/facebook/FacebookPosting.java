0b2eacb20f7cf2adb4d7e69e49b6da7edaf43e69
B00097070 <32622113+B00097070-Ryan-Noble@users.noreply.github.com>
2019-12-16 05:36:06 +0000

    Posting picture to fb feed is working. Additional string resource values made or altered

--- app/src/main/java/com/example/a2in1/ui/facebook/FacebookPosting.java
+++ app/src/main/java/com/example/a2in1/ui/facebook/FacebookPosting.java
@@ -1,8 +1,14 @@
 package com.example.a2in1.ui.facebook;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +24,25 @@ import com.example.a2in1.fragmentRedirects.FbSignInActivity;
 import com.example.a2in1.R;
 import com.facebook.CallbackManager;
 import com.facebook.Profile;
+import com.facebook.share.model.ShareContent;
 import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.ShareMediaContent;
+import com.facebook.share.model.SharePhoto;
 import com.facebook.share.widget.ShareDialog;
+
+import java.io.FileNotFoundException;
 
 import static com.example.a2in1.myPreferences.getBoolPref;
 
 public class FacebookPosting extends Fragment {
+
+    final private int getPicVal = 0;
+
+    private Button getGallImg;
+
+    private Bitmap img;
+
+    private String log = this.getClass().getSimpleName();
 
     private CallbackManager callbackManager;
 
@@ -33,54 +52,76 @@ public class FacebookPosting extends Fragment {
 
         boolean isFbLoggedIn = getBoolPref("FBLoggedIn",false,getContext());
 
-        if (isFbLoggedIn){
+        // if user isn't logged in on fb then go to the sign in fragment
+        if (!isFbLoggedIn){
+            startActivity(new Intent(getContext(), FbSignInActivity.class));
+        }
+        else {
+            callbackManager = CallbackManager.Factory.create();
 
+            getGallImg = root.findViewById(R.id.getPicBtn);
+
+            getGallImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // boolean for if the user allowed for the permission
+                    boolean hasPermission = getContext().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+                    if (hasPermission){
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, getPicVal);
+                    }else {
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 111);
+                    }
+                }
+            });
+
+            // Changes text to contain username of the user
             final CheckBox checkedText = root.findViewById(R.id.checkedText);
             String oldText =getResources().getString(R.string.postWish);
             String username = Profile.getCurrentProfile().getFirstName();
             checkedText.setText("I, "+username+ " "+ oldText);
 
-            Button PostBtn = root.findViewById(R.id.fbPostSubmitBtn);
+            // User clicks to post to the site
+            Button PostBtn = root.findViewById(R.id.postingSubmitBtn);
             PostBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (!checkedText.isChecked()) {
                         Toast.makeText(getContext(), "Must select okay!", Toast.LENGTH_SHORT).show();
                     } else {
-                        confirmPost();
+                        alertUser(getResources().getString(R.string.fb));
                     }
                 }
             });
         }
-        // if user isn't logged in on fb then go to the sign in fragment
-        else {
-            startActivity(new Intent(getContext(), FbSignInActivity.class));
-        }
         return root;
     }
 
-    private void confirmPost() {
+    private void alertUser(String site) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
         builder.setTitle(getResources().getString(R.string.confirmTitle));
-        builder.setMessage("Facebook share window will Open");
+        builder.setMessage(getResources().getString(R.string.sharingTo) + " " + site);
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
 
-                // TODO Implement other share dialogs
-                // https://developers.facebook.com/docs/sharing/android?sdk=fbsdk
-                callbackManager = CallbackManager.Factory.create();
+                // How to share a picture info learned from: https://developers.facebook.com/docs/sharing/android?sdk=fbsdk
                 ShareDialog shareDialog = new ShareDialog(getParentFragment());
 
+                if (img != null){
+                    SharePhoto photo = new SharePhoto.Builder().setBitmap(img).build();
+
+                    ShareContent shareContent = new ShareMediaContent.Builder().addMedium(photo).build();
+
+                    shareDialog.show(shareContent, ShareDialog.Mode.AUTOMATIC);
+                }
                 //Facebook Share link content builder for just text
-                ShareLinkContent content = new ShareLinkContent.Builder()
-                        .build();
-                shareDialog.show(content);
+                shareDialog.show(new ShareLinkContent.Builder().build());
             }
         });
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getContext(), getResources().getString(R.string.cancel) + "ed Post", Toast.LENGTH_SHORT).show(); // Canceled message
+                Toast.makeText(getContext(), getResources().getString(R.string.cancel) + "ed Posting", Toast.LENGTH_SHORT).show(); // Canceled message
             }
         });
         builder.setIcon(R.mipmap.upload_icon);
@@ -90,6 +131,29 @@ public class FacebookPosting extends Fragment {
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == getPicVal) {
+            if (data != null) {
+                Uri imgUri = data.getData();
+
+                try {
+                    img = BitmapFactory.decodeStream(getContext().getContentResolver().openInputStream(imgUri));
+
+                    Log.d(log, "Image chosen was made into Bitmap");
+
+                    getGallImg.setText(getResources().getString(R.string.picAdded));
+                    getGallImg.setClickable(false);
+                }
+                catch (FileNotFoundException e) {
+                    Log.e(log, e.getMessage());
+                }
+            }
+            else {
+                getGallImg.setText(getResources().getString(R.string.postPic));
+                getGallImg.setClickable(false);
+            }
+        }
+        else
+            callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
